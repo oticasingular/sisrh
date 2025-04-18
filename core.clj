a8b323dd20140f80653a9e60bd0eaedce0e59ee2
Ana Silveira <almeida.oticasingular@gmail.com>
2025-04-17 22:14:29 -0300

    ajuste de script para build

--- core.clj
+++
@@ -1,6 +0,0 @@
-(ns my-project.core
-  (:require [ring.util.response :as response]))
-
-(defn handler [request]
-  ;; Essa função recebe uma requisição HTTP e retorna uma resposta
-  (response/response "Hello from WAR!"))
--- project.clj
+++ project.clj
@@ -1,11 +1,3 @@
 (defproject my-project "0.1.0-SNAPSHOT"
   :description "A simple project"
-  :dependencies [[org.clojure/clojure "1.10.3"]
-                 [ring/ring-core "1.9.6"]
-                 [ring/ring-jetty-adapter "1.9.6"]]
-
-  :plugins [[lein-ring "0.12.6"]]
-
-  :ring {:handler my-project.core/handler
-         :war-name "my-project.war"})
-Í
+  :dependencies [[org.clojure/clojure "1.10.3"]])
