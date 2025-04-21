149554cc25a131202b0fb1944beff410c829411e
Ana Silveira <almeida.oticasingular@gmail.com>
2025-04-21 12:59:55 -0300

    ajuste no  adicionando o script do lein-ring no clojure.yml

--- project.clj
+++ project.clj
@@ -1,22 +1,18 @@
 (defproject sisrh "0.1.0-SNAPSHOT"
   :description "Aplicação de exemplo"
   :dependencies [
-    ;; Dependências do Clojure
-    [org.clojure/clojure "1.11.0"]  ;; Versão mais recente do Clojure
+    [org.clojure/clojure "1.11.0"]
 
-    ;; Dependências do Maven convertidas para Leiningen
-    [com.sun.xml.ws/jaxws-rt "2.3.5"]      ;; JAX-WS
-    [javax.servlet/javax.servlet-api "3.0.1" :scope "provided"] ;; Servlet API
-    [org.hsqldb/hsqldb "2.6.0"]            ;; Banco HSQLDB
+    [com.sun.xml.ws/jaxws-rt "2.3.5"]
+    [javax.servlet/javax.servlet-api "3.0.1" :scope "provided"]
+    [org.hsqldb/hsqldb "2.6.0"]
 
-    ;; Dependências do Ring para o servidor web
-    [ring/ring-core "1.9.3"]               ;; Core do Ring
-    [ring/ring-jetty-adapter "1.9.3"]      ;; Adapter para o servidor Jetty
-    [compojure "1.6.2"]                    ;; Framework de rotas para Clojure
-    [hiccup "1.0.5"]                       ;; Gerador de HTML
-    [org.clojure/tools.logging "1.2.3"]    ;; Para logging
+    [ring/ring-core "1.9.3"]
+    [ring/ring-jetty-adapter "1.9.3"]
+    [compojure "1.6.2"]
+    [hiccup "1.0.5"]
+    [org.clojure/tools.logging "1.2.3"]
   ]
   :plugins [[lein-ring "0.12.5"]]
-  :ring {:handler sisrh.servlet.app  ;; ou outro namespace válido
-         :war-name "sisrh.war"}
-)
+  :ring {:handler sisrh.servlet.app
+         :war-name "sisrh.war"})
