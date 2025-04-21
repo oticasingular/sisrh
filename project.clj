a25c937c8f6a66941eccdbcbb0c6108bcdf24963
Ana Silveira <almeida.oticasingular@gmail.com>
2025-04-21 13:14:58 -0300

    ajuste no  adicionando o script do lein-ring no clojure.yml

--- project.clj
+++ project.clj
@@ -1,18 +1,14 @@
-(defproject sisrh "0.1.0-SNAPSHOT"
-  :description "Aplicação de exemplo"
-  :dependencies [
-    [org.clojure/clojure "1.11.0"]
-
-    [com.sun.xml.ws/jaxws-rt "2.3.5"]
-    [javax.servlet/javax.servlet-api "3.0.1" :scope "provided"]
-    [org.hsqldb/hsqldb "2.6.0"]
-
-    [ring/ring-core "1.9.3"]
-    [ring/ring-jetty-adapter "1.9.3"]
-    [compojure "1.6.2"]
-    [hiccup "1.0.5"]
-    [org.clojure/tools.logging "1.2.3"]
-  ]
+(defproject sisrh "0.1.0"
+  :description "Projeto SISRH com integração Java e Clojure"
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [javax.servlet/javax.servlet-api "3.0.1"]
+                 [org.hsqldb/hsqldb "2.6.0"]
+                 [com.sun.xml.ws/jaxws-rt "2.3.5"]
+                 [ring/ring-core "1.9.3"]
+                 [ring/ring-jetty-adapter "1.9.3"]
+                 [compojure "1.6.2"]]
   :plugins [[lein-ring "0.12.5"]]
-  :ring {:handler sisrh.servlet.app
-         :war-name "sisrh.war"})
+  :ring {:handler sisrh.servlet.inicializar-banco/inicializar-banco-handler
+         :war-name "sisrh.war"}
+  :java-source-paths ["src/main/java"]
+  :resource-paths ["src/main/resources"])
--- src/main/java/sisrh/servlet/InicializarBancoServlet.java
+++ src/main/java/sisrh/servlet/InicializarBancoServlet.java
@@ -77,5 +77,7 @@ public class InicializarBancoServlet extends HttpServlet {
 		}
 		return contentBuilder.toString();
 	}
-
+	public void inicializarBancoViaClojure() {
+		carregarEstruturaDados();
+	}
 }
