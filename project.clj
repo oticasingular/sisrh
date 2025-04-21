41c51b9e52c750a267af5ced447df9f01fd208a7
Ana Silveira <almeida.oticasingular@gmail.com>
2025-04-21 12:51:00 -0300

    ajuste no  adicionando o script do lein-ring no clojure.yml

--- project.clj
+++ project.clj
@@ -1,4 +1,22 @@
-(defproject my-project "0.1.0-SNAPSHOT"
-  :description "A simple project"
-  :dependencies [[org.clojure/clojure "1.10.3"]])
+(defproject sisrh "0.1.0-SNAPSHOT"
+  :description "Aplicação de exemplo"
+  :dependencies [
+    ;; Dependências do Clojure
+    [org.clojure/clojure "1.11.0"]  ;; Versão mais recente do Clojure
+
+    ;; Dependências do Maven convertidas para Leiningen
+    [com.sun.xml.ws/jaxws-rt "2.3.5"]      ;; JAX-WS
+    [javax.servlet/javax.servlet-api "3.0.1" :scope "provided"] ;; Servlet API
+    [org.hsqldb/hsqldb "2.6.0"]            ;; Banco HSQLDB
+
+    ;; Dependências do Ring para o servidor web
+    [ring/ring-core "1.9.3"]               ;; Core do Ring
+    [ring/ring-jetty-adapter "1.9.3"]      ;; Adapter para o servidor Jetty
+    [compojure "1.6.2"]                    ;; Framework de rotas para Clojure
+    [hiccup "1.0.5"]                       ;; Gerador de HTML
+    [org.clojure/tools.logging "1.2.3"]    ;; Para logging
+  ]
   :plugins [[lein-ring "0.12.5"]]
+  :ring {:handler sisrh.servlet.app  ;; ou outro namespace válido
+         :war-name "sisrh.war"}
+)
